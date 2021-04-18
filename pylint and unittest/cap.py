74aa0ab55201c8daeae0be020d72d92936123a6c
Shaninath Pawar <shaninathpawar.git@gmail.com>
2021-04-18 23:03:18 +0530

    Python first commit

---
+++ pylint and unittest/cap.py
@@ -0,0 +1,6 @@
+def cap_text(text):
+    '''
+    Input a string
+    output a capitalized string
+    '''
+    return text.title()
---
+++ pylint and unittest/simple.py
@@ -0,0 +1,15 @@
+'''
+A Very Simple Script
+'''
+
+def myfunc():
+    '''
+    A Very Simple Function
+    '''
+
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
